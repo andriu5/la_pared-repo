f1192bee8b356de344189d3ca1a5a80819144ed0
andresalvearc <andres.alvear5@gmail.com>
2021-05-04 00:44:41 -0400

    Adding DataBase Model and Wall template is located in apps/wall/templates/wall/index.html

--- apps/login_registration_app/models.py
+++ apps/login_registration_app/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 import re
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
+import bcrypt
 
 class UserManager(models.Manager):
     def basic_validator(self, postData):
@@ -28,6 +29,32 @@ class UserManager(models.Manager):
             errors['release_date'] = f"Error: The release date can not be in the Future! Today is: {datetime.today().strftime('%d/%m/%Y')}"
         return errors
 
+    def wall_validator(self, postData):
+        print(postData)
+        errors = {}
+        if len(postData['post_message']) < 5:
+            errors['message_short'] = "Error: Messages should be at least 5 characters"
+        return errors
+    
+    def log_validation(self, postData):
+        errors = {}
+        try:
+            user = User.objects.get(email = postData['email'])
+        except:
+            errors['email'] = f"Email address {postData['email']} is not registered in our database!"
+            return errors
+        if not bcrypt.checkpw(postData['password'].encode(), user.password.encode()):
+            errors['password'] = "Password does not match our database!"
+        return errors
+    
+    def message_validator(self, postData):
+        print(postData)
+        errors = {}
+        half_hour_ago = datetime.today() - timedelta(minutes=30)
+        if len(postData["createdAt"]) > 0 and not User.objects.filter(message__created_at__gt=half_hour_ago):
+            errors['userMessage_half_hour_ago'] = f"Error: Solo se permite eliminar mensajes que fueron escritos en los últimos 30 minutos! La hora actual es: {datetime.now()}"
+        return errors
+
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -48,7 +75,10 @@ class Message(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey("User", related_name="messages", on_delete=models.CASCADE)
+    user = models.ForeignKey("User", related_name="message", on_delete=models.CASCADE)
+
+    objects = UserManager()
+
     def __repr__(self):
         return f"<Message object: Message: {self.message} ({self.id})>"
 
@@ -57,7 +87,10 @@ class Comment(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    message = models.ForeignKey("Message", related_name="comments", on_delete=models.CASCADE)
-    user = models.ForeignKey("User", related_name="comments", on_delete=models.CASCADE)
+    message = models.ForeignKey("Message", related_name="comment", on_delete=models.CASCADE)
+    user = models.ForeignKey("User", related_name="comment", on_delete=models.CASCADE)
+
+    objects = UserManager()
+
     def __repr__(self):
         return f"<Comments object: Comment: {self.comment} ({self.id})>"
--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -12,9 +12,11 @@ def index(request):
     if request.method == "GET":
         if 'nombre_usuario' not in request.session.keys():
             request.session['nombre_usuario'] = ""
+        if 'email_usuario' not in request.session.keys():
+            request.session['email_usuario'] = ""
         if 'dias_restantes_para_cumple_del_usuario' not in request.session.keys():
             request.session['dias_restantes_para_cumple_del_usuario'] = 0
-    return render(request, "index.html")
+    return render(request, "login_registration/index.html")
 
 def register(request):
     errors = User.objects.basic_validator(request.POST)
@@ -35,8 +37,16 @@ def register(request):
                 new_user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=pw_hash, birthday=request.POST['birthday'])
                 print(f"Info: Nuevo Usuario agregado a la base de datos.\n Nombre: {new_user.first_name} {new_user.last_name} | Email: {new_user.email} | Fecha de Cumpleaños: {new_user.birthday}")
                 messages.success(request,"Successfully registered!")
+
+                request.session['user'] = {
+                    'id': new_user.id,
+                    'name': new_user.first_name,
+                    'lastname': new_user.last_name
+                    }
+
                 # Iniciamos la sesion del usuario y aprovechandonos que estamos validando los emails, podemos hacer lo siguiente:
                 email_usuario = request.POST['email']
+                request.session['email_usuario'] = email_usuario
                 print(email_usuario)
                 # aqui da lo mismo que tengamos personas con nombres iguales, 
                 # por que sacamos el ID del email que fue previamente validado y luego obtenemos el nombre del usuario!
@@ -56,7 +66,7 @@ def register(request):
                 request.session['dias_restantes_para_cumple_del_usuario'] = calculate_days_left_for_next_birthday(**fechaNacimientoUsuario)
 
                 request.session['nombre_usuario'] = nombre_usuario
-                return redirect("success") # nunca renderizar en una publicación, ¡siempre redirigir!
+                return redirect("wall_index") # nunca renderizar en una publicación, ¡siempre redirigir!
 
 def success(request):
     if request.method == "GET":
@@ -64,25 +74,39 @@ def success(request):
         if request.session['dias_restantes_para_cumple_del_usuario'] == 0:
             request.session['dias_restantes_para_cumple_del_usuario'] = True
             #messages.add_message(request, messages.INFO, f"Happy Birthday!!")
-        return render(request, "success.html")
+        return render(request, "login_registration/success.html")
 
 def login(request):
-    # ver si el nombre de usuario proporcionado existe en la base de datos
-    user = User.objects.filter(email=request.POST['email']) # ¿Por qué usamos el filtro aquí en lugar de get?
-    if user: # tenga en cuenta que aquí aprovechamos la veracidad: una lista vacía devolverá falso
-        logged_user = user[0] 
-        # asumiendo tenemos un usuario con este nombre de usuario, éste sería el primero en la lista que obtenemos
-        # por supuesto, deberíamos tener cierta lógica para evitar duplicados de nombres cuando creamos usuarios
-        # usa el método check_password_hash de bcrypt, pasando el hash de nuestra base de datos y la contraseña del formulario
-        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
-        # si obtenemos True después de validar la contraseña, podemos poner la identificación del usuario en la sesión
-            request.session['userid'] = logged_user.id
-            # ¡Nunca renderices en una publicación, siempre redirigir!
-            messages.success(request,"Successfully logged in!")
-            return redirect('success')
-    # si no encontramos nada en la base de datos buscando por nombre de usuario o si las contraseñas no coinciden, 
-    # redirigir a una ruta segura
-    return redirect('index')
+    errors = User.objects.log_validation(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('index')
+    else:
+        # ver si el nombre de usuario proporcionado existe en la base de datos
+        user = User.objects.filter(email=request.POST['email']) # ¿Por qué usamos el filtro aquí en lugar de get?
+        if user: # tenga en cuenta que aquí aprovechamos la veracidad: una lista vacía devolverá falso
+            logged_user = user[0] 
+            user = User.objects.get(email = request.POST['email'])
+            request.session['user'] = {
+                'id': user.id,
+                'name': user.first_name,
+                'lastname': user.last_name
+            }
+            request.session['nombre_usuario'] = user.first_name
+            request.session['email_usuario'] = user.email
+            # asumiendo tenemos un usuario con este nombre de usuario, éste sería el primero en la lista que obtenemos
+            # por supuesto, deberíamos tener cierta lógica para evitar duplicados de nombres cuando creamos usuarios
+            # usa el método check_password_hash de bcrypt, pasando el hash de nuestra base de datos y la contraseña del formulario
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            # si obtenemos True después de validar la contraseña, podemos poner la identificación del usuario en la sesión
+                request.session['userid'] = logged_user.id
+                # ¡Nunca renderices en una publicación, siempre redirigir!
+                messages.success(request,"Successfully logged in!")
+                return redirect('wall_index')
+        # si no encontramos nada en la base de datos buscando por nombre de usuario o si las contraseñas no coinciden, 
+        # redirigir a una ruta segura
+        return redirect('wall_index')
 
 def logout(request):
     for key in request.session.keys(): # Imprimimos todas las claves de la session antes de borrar
